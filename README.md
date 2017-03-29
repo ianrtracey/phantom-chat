fd29276f00ee2dc78068dfff2e5c45fc8da7463d
Ian Tracey <ian.tracey@latitudeengineering.com>
2017-03-28 22:29:27 -0700

    chat basics

---
+++ README.md
@@ -0,0 +1,13 @@
+# Phantom
+Lightweight, bulletproof public chat
+
+Product Direction
+- link is the chatroom
+- you enter to a trailing list of chat
+- you can signin in order to post something
+- hashtags can be clicked to filter by them
+- search feature
+- @symbol only looks up the people in the chat
+- @ replies get sent in an email
+
+
---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.use('/static', express.static('public'));
+
+io.on('connection', function(socket) {
+  console.log('client connected');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function() {
+  console.log('listening on 3000');
+});
